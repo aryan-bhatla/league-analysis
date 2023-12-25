e85711ef99841cbd20f0531576396580f3595ed1
aryan-bhatla <aryan.bhatla184@gmail.com>
2023-12-25 19:32:56 +1100

    Added file to declare team compositions
    Will be used to calculate total team elo

---
+++ team_construction.py
@@ -0,0 +1,78 @@
+#----------------------------------------------------------------------------------------------------- #
+# Roles for each team
+#----------------------------------------------------------------------------------------------------- # 
+Support = "Support"
+ADC = "ADC"
+Middle = "Middle"
+Jungle = "Jungle"
+Top = "Top"
+
+Support_Sub = "Support_Sub"
+ADC_Sub = "ADC_Sub"
+Middle_Sub = "Middle_Sub"
+Jungle_Sub = "Jungle_Sub"
+Top_Sub = "Top_Sub"
+
+#----------------------------------------------------------------------------------------------------- # 
+# LCK teams 
+#----------------------------------------------------------------------------------------------------- # 
+Dplus_KIA = {Support: "Kellin", 
+             ADC: "Aiming", 
+             Middle: "ShowMaker", 
+             Jungle: "Lucid", 
+             Top: "Kingen"}
+
+DRX = {Support: "Pleata", 
+       ADC: "Teddy", 
+       Middle: "SeTab", 
+       Jungle: "Sponge", 
+       Top: "Rascal"}
+
+Gen_G = {Support: "Lehends", 
+         ADC: "Peyz", 
+         Middle: "Chovy", 
+         Jungle: "Canyon", 
+         Top: "Kiin"}
+
+Hanwha_Life_Esports = {Support: "Delight", 
+                       ADC: "Viper", 
+                       Middle: "Zeka", 
+                       Jungle: "Peanut", 
+                       Top: "Doran"}
+
+KT_Rolster = {Support: "BeryL", 
+              ADC: "Deft", 
+              Middle: "Bdd", 
+              Jungle: "Pyosik", 
+              Top: "PerfecT"}
+
+Kwangdong_Freecs = {Support: "Andil", 
+                    ADC: "Taeyoon", 
+                    Middle: "BuLLDog", 
+                    Jungle: "Cuzz", 
+                    Jungle_Sub: 'YoungJae', 
+                    Top: "DuDu"}
+
+Liiv_SANDBOX = {Support: "JeongHoon", 
+                ADC: "Hena", 
+                Middle: "Clozer", 
+                Jungle: "Willer", 
+                Top: "Clear"}
+
+Nongshim_RedForce = {Support: "Peter", 
+                    ADC: "Jiwoo", 
+                    Middle: "FIESTA", 
+                    Jungle: "Sylvie", 
+                    Top: "DnDn"}
+
+BRION = {Support: "Effort", 
+         ADC: "Envyy", 
+         Middle: "Karis", 
+         Jungle: "gideon", 
+         Top: "Morgan"}
+
+T1 = {Support: "Keria", 
+      ADC: "Gumayusi", 
+      Middle: "Faker", 
+      Jungle: "Oner", 
+      Top: "Zeus"}
