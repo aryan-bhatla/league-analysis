a2edb450bfacb169f499f473182ad1e9256641ea
aryan-bhatla <aryan.bhatla184@gmail.com>
2024-01-04 22:50:22 +1100

    Change Flask to listen in on 0.0.0.0 for Docker
    Now uses port 80 for any port forwarding via Docker

--- app.py
+++ app.py
@@ -35,5 +35,5 @@ def index():
                                          lec_data = lec_data)
 
 if __name__ == '__main__': 
-    app.run(debug = True)
+    app.run(debug = True, host='0.0.0.0', port = 80)
 
