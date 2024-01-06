9741939ef0207db459b2ba415d1b02599643f74e
aryan-bhatla <aryan.bhatla184@gmail.com>
2024-01-06 16:12:28 +1100

    Refactored rating calculation to include xgboost

--- rating_calculation.py
+++ rating_calculation.py
@@ -28,7 +28,7 @@ def calc_player_rating(player: str, model_results: pd.DataFrame, final_data: pd.
     except KeyError:
         if method == "coefficient":
             return 3000
-        elif method == "importance":
+        elif method == "rf_importance" or method == "xg_importance":
             return 25
 
     # Linear model
@@ -36,22 +36,33 @@ def calc_player_rating(player: str, model_results: pd.DataFrame, final_data: pd.
         coefficient_column = f"{player_role}_Avg_Coefficient"
         coefficient = model_results[coefficient_column] 
 
-        # Filter final_data for columns present in the 'coefficient' Series
+        # Filter final_data for columns present in the 'coefficient' series
         filtered_final_data = final_data.loc[:, coefficient.index]
 
         # Multiply coefficients by stats 
         stat_ratings = filtered_final_data.loc[player].mul(coefficient)
 
     # Tree model 
-    elif method == "importance": 
-        importance_column = f"{player_role}_Avg_Importance"
-        importance = model_results[importance_column]
+    elif method == "rf_importance": 
+        rf_importance_column = f"{player_role}_Avg_rf_Importance"
+        rf_importance = model_results[rf_importance_column]
 
-        # Filter final_data for columns present in the 'importance' Series
-        filtered_final_data = final_data.loc[:, importance.index]
+        # Filter final_data for columns present in the 'rf_importance' series
+        filtered_final_data = final_data.loc[:, rf_importance.index]
 
         # Multiply importance values by stats 
-        stat_ratings = filtered_final_data.loc[player].mul(importance)
+        stat_ratings = filtered_final_data.loc[player].mul(rf_importance)
+
+    # xgboost model 
+    elif method == "xg_importance":  
+        xg_importance_column = f"{player_role}_Avg_xg_Importance"
+        xg_importance = model_results[xg_importance_column]
+
+        # Filter final_data for columns present in the 'xg_importance' series
+        filtered_final_data = final_data.loc[:, xg_importance.index]
+
+        # Multiply importance values by stats 
+        stat_ratings = filtered_final_data.loc[player].mul(xg_importance)
 
     # Add all individual stats to get final rating
     rating = stat_ratings.sum()
