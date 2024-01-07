15387275fa96e4f6ba13666a5ed3569c2f058331
aryan-bhatla <aryan.bhatla184@gmail.com>
2024-01-07 13:55:33 +1100

    Consolidate Kelly bet function into logistics

--- logistics_functions.py
+++ logistics_functions.py
@@ -166,3 +166,30 @@ def winrate_to_odds(team_one_winrate: float, team_two_winrate: float) -> list[fl
     odds = [odds_one, odds_two]
 
     return odds 
+
+
+#----------------------------------------------------------------------------------------------------- # 
+# Kelly bet function
+#----------------------------------------------------------------------------------------------------- # 
+def kelly_bet(calculated_win_prob: float, odds_your_team: float, odds_other_team: float) -> float: 
+    '''
+        
+        Parameters: 
+            calculated_win_prob (float): Winrate you have calculated for a team
+            odds_your_team (float): Bookmaker odds for team you are betting on
+            odds_other_team (float): Bookmaker odds for team you are not betting on
+            
+        Returns: 
+            bet_size (float): Maximum bet size calculated via Kelly criterion 
+
+    '''
+    # Calculate win probability that bookmaker has determined 
+    implied_win_prob = odds_your_team / (odds_your_team + odds_other_team) 
+
+    # Set-up Kelly equation
+    numerator = (odds_your_team * calculated_win_prob) - implied_win_prob 
+
+    # Derive maximal bet size 
+    bet_size = numerator / odds_your_team
+
+    return bet_size 
